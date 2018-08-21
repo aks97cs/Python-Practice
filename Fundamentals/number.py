b0da86f0bb1a2401f41caa8d14c8219df6242b14
aks97cs <aks97cs@gmail.com>
2018-08-21 18:39:37 +0530

    Number

---
+++ Fundamentals/number.py
@@ -0,0 +1,27 @@
+print(2+2) #output 4
+print(50-5*6) #output 20
+print((50-5*6)/5)#output 4.0
+# Division always return a floating point number
+
+# Now lets see the difference between clasic division and flore division
+
+#here clasic division
+print(10/3) #output 3.3333333333333335 
+
+#above clasic divion return a float value
+
+#here floor division
+print(10//3) #output 3
+
+#above floor division discard the fractional part
+print(10%3) #output 1
+
+print(5**2) #output 25
+
+#here we use "**" for power of 5
+
+print(4*2.5-1) #output 9.0
+
+#There is full support for floating point; operators with 
+#mixed type operands convert the integer operand to floating point:
+
