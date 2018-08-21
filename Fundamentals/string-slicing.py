32467562adb64a97dc4b87f782be8d2febd47036
aks97cs <aks97cs@gmail.com>
2018-08-22 01:46:15 +0530

    Slicing

---
+++ Fundamentals/string-slicing.py
@@ -0,0 +1,7 @@
+word = "Python"
+print(word[0:2]) # characters from position 0 (included) to 2 (excluded)
+# output Py
+print(word[:2]) #output Py
+print(word[:]) #output Python
+print(word[-2:]) # characters from the second-last (included) to the end
+#output on
