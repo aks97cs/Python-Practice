d89832c7b1b3f1c39435d001865e81d30d78392f
aks97cs <aks97cs@gmail.com>
2018-08-22 01:29:10 +0530

    Strings

---
+++ Fundamentals/strings.py
@@ -0,0 +1,32 @@
+str1 = 'don\'t' # use \' to escape the single quote...
+print(str1)
+str2 = "don't"  # another aproach 
+print(str2)
+str3 = '"Yes," they said.'
+print(str3)
+str4 = "Anuj\nSingh"
+print(str4)
+
+print('C:\some\name')  # here \n means newline!
+
+print(r'C:\some\name')  # note the r before the quote
+
+# r means raw string
+
+print("""
+
+fhjsfjhfd
+fjdfgjldjgkf
+\fdgkdfgkd
+dgkjldfklgf
+fdgdjkl
+	""")
+
+#Strings can be concatenated (glued together) with the + operator, and repeated with *:
+
+print(3 * 'un' + 'ium') #concatenation of two string can be done with + operator
+
+word = "python"
+print(word[-1]) #output n
+print(word[-2]) #output o
+
