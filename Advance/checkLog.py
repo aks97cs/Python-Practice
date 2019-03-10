1bbd85a7110d8156f01b4ad5eeae5d34a2bc6b4b
aks97cs <aks97cs@gmail.com>
2019-03-10 18:21:02 +0530

    advance concept of python

---
+++ Advance/checkLog.py
@@ -0,0 +1,7 @@
+import logging
+logging.basicConfig(filename='temp/check.log',level=logging.DEBUG)
+if 1:
+	logging.debug("1")
+	logging.info("1")
+	logging.error("1")
+	logging.warning("1")
---
+++ Advance/whileElse_contruct.py
@@ -0,0 +1,13 @@
+'''
+@Advance python module
+@while else contruct
+'''
+i= 10
+while 4<20:
+    print(i)
+    i = i+1
+    # break
+    #apply break condition here to see output effect
+else:
+    print('working')
+print("really working")
